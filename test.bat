1bb084f82c126294eb46cae96f978a70ebd2430d
Jip Spinnewijn <jip.spinnewijn@gmail.com>
2015-02-01 14:06:26 +0100

    Added uninstall command to test.bat

--- test.bat
+++ test.bat
@@ -1,3 +1,4 @@
 adb connect 192.168.1.50:4321
+adb -s 192.168.1.50:4321 shell pm uninstall nl.spip.AndroidTVOpenFLExample
 adb -s 192.168.1.50:4321 install -r bin/android/bin/bin/AndroidTVOpenFLExample-debug.apk
 adb -s 192.168.1.50:4321 shell am start -n nl.spip.AndroidTVOpenFLExample/nl.spip.AndroidTVOpenFLExample.MainActivity
